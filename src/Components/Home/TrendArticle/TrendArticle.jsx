40841f7f167851dd83fee69f9c42e3a3bf69910f
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-11-27 09:42:32 +0600

    react scrolling component added

--- src/Components/Home/TrendArticle/TrendArticle.jsx
+++ src/Components/Home/TrendArticle/TrendArticle.jsx
@@ -18,19 +18,17 @@ const TrendArticle = ({article}) => {
                 <figure><img className="h-60 w-full"  src={image} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title text-center font-bold text-2xl text-red-600">{title}</h2>
-                    <p className="text-justify">{des} <Link to={`serviceDetails/${_id}`}><button className="text-base font-bold text-red-600">View Details</button></Link></p>
-                    <div className="flex  mt-5">
+                    <p className="text-justify">{des} <Link to={`serviceDetails/${_id}`}><button className="text-base font-bold text-red-600">Details</button></Link></p>
+                    <div className="flex ">
                         <div>
                            {/* <img  className="h-10 w-12" src={service_provider_image} alt="" /> */}
                         </div>
                         <div>
-                            <h1 className="ml-3  mt-2 text-red-600 font-bold">{publisher}</h1>
+                            <h1 className="ml-3  text-red-600 font-bold">{publisher}</h1>
                         </div>
                     </div>
                     
-                    {/* <div>
-                        <h1 className="mt-2 font-bold text-base"><span className="font-bold text-orange-600">Price:</span> $ {service_price} only</h1>
-                    </div> */}
+                    
                     
                 </div>
             </div> 
