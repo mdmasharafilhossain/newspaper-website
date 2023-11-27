057a0ce1f8605b39a1356bbcb66ac9979ff7f731
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-11-27 20:50:15 +0600

    all articles page added

---
+++ src/Components/AllArtciles/AllArticles.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+
+
+const AllArticles = ({article}) => {
+    const {image,title,_id,publisher,description,color,name} = article;
+    const LimitDes = (description, limit) => {
+        const words = description.split(' ');
+        if (words.length > limit) {
+            return words.slice(0, limit).join(' ') + '.......';
+        }
+        return description;
+    };
+    const des = LimitDes(description, 20);
+    return (
+        <div>
+            <div>
+                <div className={`card w-96 h-[600px] border  rounded-none bg-base-100 shadow-2xl`}>
+                <figure><img className="h-60 w-full"  src={image} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title text-center font-bold text-2xl text-red-600">{title}</h2>
+                    <p className="text-justify">{des} <Link to={`serviceDetails/${_id}`}><button className="text-base font-bold text-red-600">Details</button></Link></p>
+                    <div className="flex ">
+                        <div>
+                           {/* <img  className="h-10 w-12" src={service_provider_image} alt="" /> */}
+                        </div>
+                        <div>
+                            <h1 className="ml-3  text-red-600 font-bold">{publisher}</h1>
+                        </div>
+                        
+                    </div>
+                    
+                    
+                    
+                </div>
+            </div> 
+                </div>
+        </div>
+    );
+};
+
+export default AllArticles;
--- src/Components/Home/Navbar/AllArticle/AllArticle.jsx
+++ src/Components/Home/Navbar/AllArticle/AllArticle.jsx
@@ -1,9 +1,58 @@
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import AllArticles from "../../../AllArtciles/AllArticles";
 
 
 const AllArticle = () => {
+    const [art, setart] = useState([]);
+    const [search,setSearch] = useState('');
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/article?search=${search}`)
+            .then(res => res.json())
+            .then(data => setart(data))
+    }, [search]);
+
+    const handleSerach = e =>{
+        e.preventDefault();
+        const searchText = e.target.search.value;
+        console.log(searchText);
+        setSearch(searchText)
+    }
+
     return (
-        <div>
-            <h1>All Article</h1>
+        <div className="mt-20">
+            <div className="mx-auto text-center w-4/12 ">
+                <h1 className="text-5xl text-red-700 border-red-400 text-center font-bold  mb-12 py-4">All Article </h1>
+            </div>
+
+            {/* <div tabIndex={0} className="collapse  w-[150px] ml-44 mb-10 collapse-arrow border border-base-300 bg-base-200">
+                <div className="collapse-title text-xl font-medium">
+                  Filter by?
+                </div>
+                <div className="collapse-content">
+                  <div>
+                  <button className="btn border ">Tags</button>
+                  </div>
+                 <div>
+                 <button className="btn border ">Publisher</button>
+                 </div>
+                </div>
+            </div> */}
+            <form onSubmit={handleSerach} className="container  ml-[450px] mb-20">
+                <input className="border-2 w-1/2 py-2 border-red-600 rounded-lg" type="text" name="search" placeholder="Search Any Article..." id=""/>
+                <input className="btn bg-red-700 hover:bg-red-600 text-yellow-50" type="submit" value="Search" />
+            </form>
+
+            <div className="grid grid-cols-3 gap-10  container mx-auto">
+                {
+                    art.map(article => <InfiniteScroll dataLength={art.length} key={article.id} article={article}>
+
+                        <AllArticles article={article}></AllArticles>
+
+                    </InfiniteScroll>)
+                }
+            </div>
         </div>
     );
 };
