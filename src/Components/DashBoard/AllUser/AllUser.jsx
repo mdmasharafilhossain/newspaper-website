25535bab8d0d5ad88bb9f1dba9a99da109ad8648
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-11-28 18:13:17 +0600

    All User Section added

--- src/Components/DashBoard/AllUser/AllUser.jsx
+++ src/Components/DashBoard/AllUser/AllUser.jsx
@@ -1,11 +1,67 @@
 
+import { useQuery } from '@tanstack/react-query';
+import UseAxiosSecure from './../../Hooks/UseAxiosSecure/UseAxiosSecure';
 
 const AllUser = () => {
+    const axiosSecure = UseAxiosSecure();
+    const { data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/users');
+            return res.data;
+
+        }
+
+    })
     return (
         <div>
             <div className="flex justify-evenly my-6">
                 <h2 className="text-3xl ">All User</h2>
-                <h2 className="text-3xl ">Total User</h2>
+                <h2 className="text-3xl ">Total User:{users.length}</h2>
+            </div>
+            <div className="overflow-x-auto ml-10">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            users.map(user => <tr key={user._id}>
+                                
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={user.photo} />
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </td>
+                                <td>
+                                    <h1>{user.name}</h1>
+                                    <br />
+                                    
+                                </td>
+                                <td>{user.email}</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">Make Admin</button>
+                                </th>
+                            </tr>)
+                        }
+                        
+                    </tbody>
+                    {/* foot */}
+                 
+                </table>
             </div>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import {
-  
+
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
 import { router } from './Routes/Routes.jsx';
 import AuthProviders from './Components/AuthProviders/AuthProviders.jsx';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProviders><RouterProvider router={router} /></AuthProviders>
+    <AuthProviders>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+
+    </AuthProviders>
   </React.StrictMode>,
 )
