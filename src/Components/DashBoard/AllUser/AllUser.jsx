8fc9e31383cc8a7d4c7759d5ced7729415bf15a4
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-11-28 15:49:57 +0600

    axiosSecure hooks added

--- src/Components/DashBoard/AllUser/AllUser.jsx
+++ src/Components/DashBoard/AllUser/AllUser.jsx
@@ -3,7 +3,10 @@
 const AllUser = () => {
     return (
         <div>
-            <h2>All User List</h2>
+            <div className="flex justify-evenly my-6">
+                <h2 className="text-3xl ">All User</h2>
+                <h2 className="text-3xl ">Total User</h2>
+            </div>
         </div>
     );
 };
