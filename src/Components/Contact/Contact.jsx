b9bd5d740ebcbbc43a1f6d97eff1ab9c0a77f74d
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-12-15 18:37:25 +0600

    contact us section added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -21,20 +21,20 @@ const Contact = () => {
           });
       };
     return (
-        <div className="mt-20">
+        <div className="mt-10 container mx-auto mb-10">
             <h1 className="text-5xl text-red-700 border-red-400 text-center font-bold  mb-12 py-4">Contact Us</h1>
-            <form className='border-4 shadow-xl  border-orange-600 lg:p-20 lg:mr-72 lg:ml-96 mt-20' ref={form} onSubmit={sendEmail}>
-           <h2 className='text-4xl font-bold text-center mb-10 text-orange-600'>Contact Me</h2>
+            <form className='border-4 shadow-xl  border-red-600 lg:p-20 lg:mr-72 lg:ml-96 mt-20' ref={form} onSubmit={sendEmail}>
+           <h2 className='text-4xl font-bold text-center mb-10 text-red-600'>Contact Us</h2>
       <div className='flex md:gap-32 lg:gap-16'>
-      <label className="form-control w-full max-w-xs">
+      <label className="form-control  w-full max-w-xs">
   <div className="label">
     <span className="label-text">Name</span>
     
   </div>
-  <input type="text" placeholder="Your Name" name="from_name" className="input input-bordered w-full max-w-xs" />
+  <input type="text" placeholder="Your Name" name="from_name" className="input input-bordered w-full " />
   
 </label>
-      <label className="form-control w-full max-w-xs">
+      <label className="form-control mr-1   w-full max-w-xs">
   <div className="label">
     <span className="label-text">Email</span>
     
@@ -53,7 +53,7 @@ const Contact = () => {
   <textarea className="textarea textarea-bordered h-24" name="message" placeholder="Send Me Message"></textarea>
   
 </label>
-      <input className='btn bg-orange-600 text-white w-1/2 ml-24 md:ml-44 lg:ml-40 mt-5' type="submit" value="Send" />
+      <input className='btn bg-red-600 text-white w-1/2 ml-24 md:ml-44 lg:ml-40 mt-5' type="submit" value="Send" />
     </form>
         </div>
     );
